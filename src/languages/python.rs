9e38200b1a110d41fc212522d10f95ccd5ca03ef
j178 <10510431+j178@users.noreply.github.com>
2024-11-03 17:24:25 +0800

    Partition filenames and run delegate to multiple subprocesses

--- src/languages/python.rs
+++ src/languages/python.rs
@@ -1,8 +1,13 @@
+use std::cmp::max;
+use std::os::unix::process::ExitStatusExt;
 use std::path::{Path, PathBuf};
-use std::process::Output;
+use std::process::{ExitStatus, Output};
+use std::sync::Arc;
 
+use anyhow::Ok;
 use assert_cmd::output::{OutputError, OutputOkExt};
 use tokio::process::Command;
+use tokio::task::JoinSet;
 
 use crate::config;
 use crate::hook::Hook;
@@ -64,7 +69,10 @@ impl LanguageImpl for Python {
 
     async fn run(&self, hook: &Hook, filenames: &[&String]) -> anyhow::Result<Output> {
         // Construct the `PATH` environment variable.
-        let env = hook.environment_dir().unwrap();
+        let env = hook
+            .environment_dir()
+            .expect("No environment dir for Python");
+        let cmds = shlex::split(&hook.entry).ok_or(anyhow::anyhow!("Failed to parse entry"))?;
 
         let new_path = std::env::join_paths(
             std::iter::once(bin_dir(env.as_path())).chain(
@@ -75,22 +83,99 @@ impl LanguageImpl for Python {
             ),
         )?;
 
-        // TODO: handle signals
-        // TODO: better error display
-        let cmds = shlex::split(&hook.entry).ok_or(anyhow::anyhow!("Failed to parse entry"))?;
-        let output = Command::new(&cmds[0])
-            .args(&cmds[1..])
-            .args(&hook.args)
-            .args(filenames)
-            .env("VIRTUAL_ENV", &env)
-            .env("PATH", new_path)
-            .env_remove("PYTHONHOME")
-            .stderr(std::process::Stdio::inherit())
-            .output()
-            .await?;
+        let concurrency = if hook.require_serial {
+            1
+        } else {
+            // read from config, and the count of cpus
+            12
+        };
+        let partitions = partitions(hook, filenames, concurrency);
+        let semaphore = Arc::new(tokio::sync::Semaphore::new(
+            concurrency.min(partitions.len()),
+        ));
 
-        Ok(output)
+        let mut tasks = JoinSet::new();
+        for batch in partitions {
+            let semaphore = semaphore.clone();
+
+            let cmds = cmds.clone();
+            let hook_args = hook.args.clone();
+            let new_path = new_path.clone();
+            let env = env.clone();
+            let batch = batch
+                .into_iter()
+                .map(ToString::to_string)
+                .collect::<Vec<_>>();
+
+            tasks.spawn(async move {
+                let _permit = semaphore
+                    .acquire()
+                    .await
+                    .map_err(|_| anyhow::anyhow!("Semaphore error"))?;
+
+                // TODO: handle signals
+                // TODO: better error display
+                Command::new(&cmds[0])
+                    .args(&cmds[1..])
+                    .args(&hook_args)
+                    .args(batch)
+                    .env("VIRTUAL_ENV", &env)
+                    .env("PATH", new_path)
+                    .env_remove("PYTHONHOME")
+                    .stderr(std::process::Stdio::inherit())
+                    .output()
+                    .await
+                    .map_err(|e| anyhow::anyhow!("Error running command: {:?}", e))
+            });
+        }
+
+        let mut combined_status = 0;
+        let mut combined_stdout = Vec::new();
+
+        while let Some(result) = tasks.join_next().await {
+            let output = result??;
+            combined_status |= output.status.code().unwrap_or(1);
+            combined_stdout = output.stdout;
+        }
+
+        Ok(Output {
+            status: ExitStatus::from_raw(combined_status),
+            stdout: combined_stdout,
+            stderr: vec![],
+        })
     }
+}
+
+fn partitions<'a>(
+    hook: &'a Hook,
+    filenames: &'a [&String],
+    concurrency: usize,
+) -> Vec<Vec<&'a String>> {
+    let max_per_batch = max(4, filenames.len() / concurrency);
+    let max_cli_length = 1 << 12;
+    let command_length = hook.entry.len() + hook.args.iter().map(String::len).sum::<usize>();
+    // TODO: env size
+
+    let mut partitions = Vec::new();
+    let mut current = Vec::new();
+    let mut current_length = command_length + 1;
+
+    for &filename in filenames {
+        let length = filename.len();
+        if current_length + length > max_cli_length || current.len() >= max_per_batch {
+            partitions.push(current);
+            current = Vec::new();
+            current_length = 0;
+        }
+        current.push(filename);
+        current_length += length;
+    }
+
+    if !current.is_empty() {
+        partitions.push(current);
+    }
+
+    partitions
 }
 
 fn bin_dir(venv: &Path) -> PathBuf {
