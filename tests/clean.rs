eeea893503d6bdf9c9d2c1825291475abc494cd2
j178 <10510431+j178@users.noreply.github.com>
2024-11-10 15:57:48 +0800

    Fix tests

--- tests/clean.rs
+++ tests/clean.rs
@@ -1,5 +1,7 @@
+use assert_fs::assert::PathAssert;
+use assert_fs::fixture::{PathChild, PathCreateDir};
+
 use crate::common::{cmd_snapshot, TestContext};
-use assert_fs::fixture::{FileWriteStr, PathChild};
 
 mod common;
 
@@ -7,53 +9,19 @@ mod common;
 fn clean() -> anyhow::Result<()> {
     let context = TestContext::new();
 
-    context.init_project();
-    context
-        .workdir()
-        .child(".pre-commit-config.yaml")
-        .write_str(indoc::indoc! {r"
-            repos:
-              - repo: https://github.com/pre-commit/pre-commit-hooks
-                rev: v5.0.0
-                hooks:
-                  - id: trailing-whitespace
-                  - id: end-of-file-fixer
-                  - id: check-json
-        "})?;
+    let home = context.workdir().child("home");
+    home.create_dir_all()?;
 
-    cmd_snapshot!(context.filters(), context.run(), @r#"
+    cmd_snapshot!(context.filters(), context.clean().env("PRE_COMMIT_HOME", &*home), @r#"
     success: true
     exit_code: 0
     ----- stdout -----
-    Cloning https://github.com/pre-commit/pre-commit-hooks@v5.0.0
-    Installing environment for https://github.com/pre-commit/pre-commit-hooks@v5.0.0
-    trim trailing whitespace.............................(no files to check)Skipped
-    fix end of files.....................................(no files to check)Skipped
-    check json...........................................(no files to check)Skipped
-
-    ----- stderr -----
-    "#);
-    cmd_snapshot!(context.filters(), context.clean(), @r#"
-    success: true
-    exit_code: 0
-    ----- stdout -----
-    Cleaned `[HOME]/`
+    Cleaned `home`
 
     ----- stderr -----
     "#);
 
-    cmd_snapshot!(context.filters(), context.run(), @r#"
-    success: true
-    exit_code: 0
-    ----- stdout -----
-    Cloning https://github.com/pre-commit/pre-commit-hooks@v5.0.0
-    Installing environment for https://github.com/pre-commit/pre-commit-hooks@v5.0.0
-    trim trailing whitespace.............................(no files to check)Skipped
-    fix end of files.....................................(no files to check)Skipped
-    check json...........................................(no files to check)Skipped
-
-    ----- stderr -----
-    "#);
+    home.assert(predicates::path::missing());
 
     Ok(())
 }
