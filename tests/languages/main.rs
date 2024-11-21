7f57f6ede92e61983c654a0d9c5e8db1e958aaf2
Lemon <bxb100@outlook.com>
2024-11-21 18:30:47 +0800

    ci: dynamic CI demo

--- tests/languages/main.rs
+++ tests/languages/main.rs
@@ -1,6 +1,7 @@
 #[path = "../common/mod.rs"]
 mod common;
 
-#[cfg(all(feature = "docker", target_os = "linux"))]
+// #[cfg(all(feature = "docker", target_os = "linux"))]
+#[cfg(feature = "docker-ci")]
 mod docker;
 mod fail;
