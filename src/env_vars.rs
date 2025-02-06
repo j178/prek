5eba11a58436e18de70228c450c530caa4d02daf
j178 <10510431+j178@users.noreply.github.com>
2025-02-06 17:44:48 +0800

    fmt

--- src/env_vars.rs
+++ src/env_vars.rs
@@ -7,7 +7,8 @@ impl EnvVars {
 
     pub const PREFLIGIT_HOME: &'static str = "PREFLIGIT_HOME";
 
-    pub const PREFLIGIT_INTERNAL__SORT_FILENAMES: &'static str = "PREFLIGIT_INTERNAL__SORT_FILENAMES";
+    pub const PREFLIGIT_INTERNAL__SORT_FILENAMES: &'static str =
+        "PREFLIGIT_INTERNAL__SORT_FILENAMES";
 
     // Pre-commit specific environment variables
     pub const PRE_COMMIT_HOME: &'static str = "PRE_COMMIT_HOME";
