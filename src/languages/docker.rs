a9f0f1c66b4100387ffd78bb55ed8ef96f0f18fc
Jo <10510431+j178@users.noreply.github.com>
2025-07-31 21:58:32 +0800

    Remove language resolve step

--- src/languages/docker.rs
+++ src/languages/docker.rs
@@ -10,7 +10,7 @@ use seahash::SeaHasher;
 use tracing::trace;
 
 use crate::fs::CWD;
-use crate::hook::{Hook, ResolvedHook};
+use crate::hook::{Hook, InstalledHook};
 use crate::languages::LanguageImpl;
 use crate::process::Cmd;
 use crate::run::run_by_batch;
@@ -22,14 +22,14 @@ const PRE_COMMIT_LABEL: &str = "PRE_COMMIT";
 pub struct Docker;
 
 impl Docker {
-    fn docker_tag(hook: &ResolvedHook) -> String {
+    fn docker_tag(hook: &InstalledHook) -> String {
         let mut hasher = SeaHasher::new();
         hook.hash(&mut hasher);
         let digest = crate::store::to_hex(hasher.finish());
         format!("prefligit-{digest}")
     }
 
-    async fn build_docker_image(hook: &ResolvedHook, pull: bool) -> Result<()> {
+    async fn build_docker_image(hook: &InstalledHook, pull: bool) -> Result<()> {
         let Some(src) = hook.repo_path() else {
             anyhow::bail!("Language `docker` cannot work with `local` repository");
         };
@@ -167,21 +167,13 @@ impl Docker {
 }
 
 impl LanguageImpl for Docker {
-    async fn resolve(&self, hook: &Hook, _store: &Store) -> Result<ResolvedHook> {
-        Ok(ResolvedHook::NoNeedInstall(hook.clone()))
-    }
-
-    async fn install(&self, hook: &ResolvedHook, _store: &Store) -> Result<()> {
+    async fn install(&self, hook: &Hook, _store: &Store) -> Result<InstalledHook> {
+        let hook = InstalledHook::NoNeedInstall(hook.clone());
         let env = hook.env_path().expect("Docker must have env path");
 
-        // TODO: check unsupported language version
-        if !hook.additional_dependencies.is_empty() {
-            anyhow::bail!("Docker does not support additional dependencies");
-        }
-
-        Docker::build_docker_image(hook, true).await?;
+        Docker::build_docker_image(&hook, true).await?;
         fs_err::tokio::create_dir_all(env).await?;
-        Ok(())
+        Ok(hook)
     }
 
     async fn check_health(&self) -> Result<()> {
@@ -190,7 +182,7 @@ impl LanguageImpl for Docker {
 
     async fn run(
         &self,
-        hook: &ResolvedHook,
+        hook: &InstalledHook,
         filenames: &[&String],
         env_vars: &HashMap<&'static str, String>,
         _store: &Store,
