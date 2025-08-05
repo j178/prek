51ca7a41ff8db3b75963b03700e4b32ac5cd094e
Jo <10510431+j178@users.noreply.github.com>
2025-08-05 19:09:56 +0800

    Add a remote test

--- tests/languages/golang.rs
+++ tests/languages/golang.rs
@@ -162,3 +162,31 @@ fn additional_dependencies() {
     ----- stderr -----
     "#);
 }
+
+/// Test a remote go hook.
+#[test]
+fn remote_hook() {
+    let context = TestContext::new();
+    context.init_project();
+    context.write_pre_commit_config(indoc::indoc! {r"
+        repos:
+          - repo: https://github.com/prefligit-test-repos/golang-hooks
+            rev: main
+            hooks:
+              - id: echo
+                verbose: true
+        "});
+    context.git_add(".");
+
+    cmd_snapshot!(context.filters(), context.run(), @r#"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+    echo.....................................................................Passed
+    - hook id: echo
+    - duration: [TIME]
+      .pre-commit-config.yaml
+
+    ----- stderr -----
+    "#);
+}
