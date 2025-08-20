a5ee2a284af1c7037f9d9bb4179adeb741342200
Jo <10510431+j178@users.noreply.github.com>
2025-08-20 18:01:31 +0800

    Tweak message

--- src/config.rs
+++ src/config.rs
@@ -752,22 +752,40 @@ pub fn read_config(path: &Path) -> Result<Config, Error> {
     }
 
     // Check for mutable revs and warn the user.
-    for repo_config in &config.repos {
-        if let Repo::Remote(repo) = repo_config {
-            let rev = &repo.rev;
-            // A rev is considered mutable if it doesn't contain a '.' (like a version)
-            // and is not a hexadecimal string (like a commit SHA).
-            if !rev.contains('.') && !is_sha(rev) {
-                warn_user!(
-                    "The `rev` field of repo `{}` appears to be a mutable reference \
-                   (moving tag / branch). Mutable references are never updated after first \
-                   install and are not supported. See \
-                   https://pre-commit.com/#using-the-latest-version-for-a-repository \
-                   for more details. Hint: `prek autoupdate` often fixes this.",
-                    repo.repo
-                );
+    let repos_has_mutable_rev = config
+        .repos
+        .iter()
+        .filter_map(|repo| {
+            if let Repo::Remote(repo) = repo {
+                let rev = &repo.rev;
+                // A rev is considered mutable if it doesn't contain a '.' (like a version)
+                // and is not a hexadecimal string (like a commit SHA).
+                if !rev.contains('.') && !looks_like_sha(rev) {
+                    return Some(repo);
+                }
             }
-        }
+            None
+        })
+        .collect::<Vec<_>>();
+    if !repos_has_mutable_rev.is_empty() {
+        let msg = repos_has_mutable_rev
+            .iter()
+            .map(|repo| format!("`{}`: {}", repo.repo.cyan(), repo.rev.yellow()))
+            .collect::<Vec<_>>()
+            .join("\n");
+
+        warn_user!(
+            "{}",
+            indoc::formatdoc! { r#"
+            The following repos have mutable `rev` fields (moving tag / branch):
+            {}
+            Mutable references are never updated after first install and are not supported.
+            See https://pre-commit.com/#using-the-latest-version-for-a-repository for more details.
+            Hint: `prek autoupdate` often fixes this",
+            "#,
+            msg
+            }
+        );
     }
 
     Ok(config)
@@ -782,7 +800,7 @@ pub fn read_manifest(path: &Path) -> Result<Manifest, Error> {
 }
 
 /// Check if a string looks like a git SHA
-fn is_sha(s: &str) -> bool {
+fn looks_like_sha(s: &str) -> bool {
     static SHA_RE: OnceLock<Regex> = OnceLock::new();
     let re = SHA_RE.get_or_init(|| Regex::new(r"^[a-fA-F0-9]+$").unwrap());
     re.is_match(s).unwrap_or(false)
