65c202ea768bbe3f82ad7639d2303e4a9a5bc4bc
Jo <10510431+j178@users.noreply.github.com>
2026-01-16 13:35:00 +0800

    Refactor

--- crates/prek/src/hooks/builtin_hooks/check_json5.rs
+++ crates/prek/src/hooks/builtin_hooks/check_json5.rs
@@ -1,25 +1,17 @@
 use std::path::Path;
 
-use futures::StreamExt;
-
 use crate::hook::Hook;
+use crate::hooks::run_concurrent_file_checks;
 use crate::run::CONCURRENCY;
 
-pub(crate) async fn check_json5(hook: &Hook, filenames: &[&Path]) -> anyhow::Result<(i32, Vec<u8>)> {
-    let mut tasks = futures::stream::iter(filenames)
-        .map(async |filename| check_file(hook.project().relative_path(), filename).await)
-        .buffered(*CONCURRENCY);
-
-    let mut code = 0;
-    let mut output = Vec::new();
-
-    while let Some(result) = tasks.next().await {
-        let (c, o) = result?;
-        code |= c;
-        output.extend(o);
-    }
-
-    Ok((code, output))
+pub(crate) async fn check_json5(
+    hook: &Hook,
+    filenames: &[&Path],
+) -> anyhow::Result<(i32, Vec<u8>)> {
+    run_concurrent_file_checks(filenames, *CONCURRENCY, |filename| {
+        check_file(hook.project().relative_path(), filename)
+    })
+    .await
 }
 
 async fn check_file(file_base: &Path, filename: &Path) -> anyhow::Result<(i32, Vec<u8>)> {
