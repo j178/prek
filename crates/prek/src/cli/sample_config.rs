37439379ed0de156bbfcd32d3678a8d8a7225423
Thaddeus Crews <repiteo@outlook.com>
2026-08-05 11:26:09 -0500

    Add `yaml-language-server:` comment to YAML sample config

--- crates/prek/src/cli/sample_config.rs
+++ crates/prek/src/cli/sample_config.rs
@@ -12,6 +12,7 @@ use crate::printer::Printer;
 
 static SAMPLE_CONFIG_YAML: &str = indoc::indoc! {"
 # See https://prek.j178.dev for more information.
+# yaml-language-server: $schema=https://www.schemastore.org/prek.json
 repos:
   - repo: https://github.com/pre-commit/pre-commit-hooks
     rev: v6.0.0
